9497f1f2cc8274f3c02053e27297d9767a1293b1
sagar <sagar.bhatt@rtcamp.com>
2017-04-20 18:43:28 +0530

    Add infinite scroll on single post
    URL will be change dynamically

--- src/components/post.vue
+++ src/components/post.vue
@@ -6,61 +6,136 @@
 
 		<div class="row rt-main" v-if="loaded === 'true'">
 
-			<div class="medium-12 small-12 column" >
+			<div class="medium-12 small-12 column" v-for="data in post">
 
 				<div class="rt-post">
 
-					<h2 class="rt-post-title"> {{ post.title.rendered }}</h2>
+					<h2 class="rt-post-title"><router-link :to="{ name: 'post', params: { name:data.slug }}"> {{ data.title.rendered }} </router-link> </h2>
 
 
-					<div class="rt-post-content rt-content" v-html="post.content.rendered" ></div>
+					<div class="rt-post-content rt-content" v-html="data.content.rendered" ></div>
 
 
 				</div>
 
 			</div>
 
+			<div class="medium-12 small-12 column" v-for="data in posts" v-if="data.id != mainPage">
+
+			<div class="rt-post">
+
+				<h2 class="rt-post-title"><router-link :to="{ name: 'post', params: { name:data.slug }}"> {{ data.title.rendered }} </router-link> </h2>
+
+
+				<div class="rt-post-content rt-content" v-html="data.content.rendered" ></div>
+
+
+			</div>
+
+			</div>
+
+		<infinite-loading :on-infinite="onInfinite" ref="infiniteLoading" :key="post">
+			<span slot="no-more">
+			There is no more data :(
+			</span>
+		</infinite-loading>
 		</div>
-
-
 
 </template>
 
 <script>
+import InfiniteLoading from 'vue-infinite-loading'
 export default {
+
+	components: {
+			InfiniteLoading,
+	},
 
 	mounted: function() {
 
-			this.getPost();
+			this.getMainPost();
 	},
 
 	data() {
 		return {
 			base_path: rtwp.base_path,
-			post:{},
+			post:[],
 			loaded:'false',
-			pageTitle:''
-
+			postPerPage: '5',
+			currentPage: '0',
+			totalPages: '',
+			mainPage:'',
+			posts:[],
 		}
 	},
 
 	methods: {
-		getPost:function () {
+		getMainPost:function () {
 
 			var vm = this;
 			wp.api.loadPromise.done( function() {
 
 				var post = new wp.api.models.Post( );
-				post.fetch( { data: { slug:vm.$route.params.name } } ).done( function (data) {
-					//console.log( data );
-					vm.post = data[0];
+				post.fetch( { data: { slug:vm.$route.params.name } } ).done( function ( data, status, header ) {
+
+					vm.post = vm.post.concat( data );
+					vm.totalPages = header.getResponseHeader( 'X-WP-TotalPages');
+					vm.mainPage = vm.post[0].id;
 					vm.loaded = 'true';
-					vm.pageTitle = vm.post.title.rendered;
-					vm.$store.commit( 'rtChangeTitle', vm.pageTitle );
+
 				});
 			});
 
+		},
+		getPost:function (pageNumber=1) {
+
+			var vm = this;
+			wp.api.loadPromise.done( function() {
+
+				var post = new wp.api.models.Post( );
+				post.fetch( { data: { per_page: vm.postPerPage,page:pageNumber } } ).done( function ( data, status, header ) {
+
+					vm.posts = vm.posts.concat( data );
+					vm.totalPages = header.getResponseHeader( 'X-WP-TotalPages');
+					vm.currentPage = parseInt( pageNumber );
+					vm.$refs.infiniteLoading.$emit('$InfiniteLoading:loaded');
+				});
+			});
+
+		},
+		onInfinite:function () {
+
+			var vm = this;
+			if( parseInt( vm.currentPage )  < parseInt( vm.totalPages ) ) {
+
+				vm.currentPage = parseInt( vm.currentPage ) + 1;
+				vm.getPost( vm.currentPage );
+
+			} else {
+				vm.$refs.infiniteLoading.$emit('$InfiniteLoading:complete');
+			}
+
+		},
+		handleScroll:function () {
+			var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+			var windowPosition = jQuery(window).scrollTop();
+			jQuery('h2').not('.rtvisited').each( function () {
+				var el = this;
+				var pos = $( el ).offset().top -windowPosition;
+				var link = $( el ).children('a').attr('href');
+				if ( parseInt(pos) >= 0 && parseInt(pos) <= 10 ) {
+					history.pushState(null, null, link );
+					jQuery( el ).addClass( "rtvisited" );
+
+				}
+			})
 		}
+	},
+	created () {
+		window.addEventListener('scroll', this.handleScroll);
+	},
+	destroyed () {
+		window.removeEventListener('scroll', this.handleScroll);
 	}
 }
 </script>
