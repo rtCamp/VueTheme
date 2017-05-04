b6a1b304235dd4f1496090ea43bbcabb96da1385
sagar <sagar.bhatt@rtcamp.com>
2017-05-04 15:06:53 +0530

    Fix js lint issue

--- src/main.js
+++ src/main.js
@@ -1,74 +1,73 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Vuex from 'vuex';
-import {HTTP as axios} from './axios/axios';
+import { HTTP as axios } from './axios/axios';
 import VueProgressiveImage from 'progressive-image/dist/vue';
 
-
 Vue.prototype.$http = axios;
-//Use vue route
 
-Vue.use( Vuex )
-Vue.use( VueRouter )
-Vue.use(VueProgressiveImage, {
+Vue.use( Vuex );
+Vue.use( VueRouter );
+Vue.use( VueProgressiveImage, {
 	removePreview: true
-})
+} );
 
 Vue.config.debug = true;
 Vue.config.devTools = true;
 
-//Import all vue compononets
+//Import all vue components
 import posts from './components/posts.vue';
-Vue.component('posts', posts)
+Vue.component( 'posts', posts );
 import post from './components/post.vue';
-Vue.component('post', post)
+Vue.component( 'post', post );
 import header from './components/theme-header.vue';
-Vue.component('theme-header', header)
+Vue.component( 'theme-header', header );
 import footer from './components/theme-footer.vue';
-Vue.component('theme-footer', footer)
+Vue.component( 'theme-footer', footer );
 import page from './components/page.vue';
+Vue.component( 'page', page );
 
 //Create main vue component
-var App = Vue.extend({
-	template:'<div id="page" class="hfeed site"><theme-header></theme-header>' +
+const App = Vue.extend( {
+	template: '<div id="page" class="hfeed site"><theme-header></theme-header>' +
 			'<main><router-view></router-view></main>' +
 			'<theme-footer></theme-footer></div>',
-	computed:{
+	computed: {
 	}
-})
+} );
 
 //Define route for vue app
 //ref : http://router.vuejs.org/en/
-const router = new VueRouter({
+const router = new VueRouter( {
 	mode: 'history',
 	base: __dirname,
 	routes: [
 
-		{ path: '/blog/:page(\\d+)?', name:'home',component: posts },
-		{ path: '/blog/:name', name:'post', component: post },
-		{ path: '/page/:name', name:'page', component: page },
+		{ path: '/blog/:page(\\d+)?', name: 'home', component: posts },
+		{ path: '/blog/:name', name: 'post', component: post },
+		{ path: '/page/:name', name: 'page', component: page },
 		{ path: '/', redirect: '/blog' },
 
 	]
-})
+} );
 
 //Define vuex store
-const store = new Vuex.Store({
+const store = new Vuex.Store( {
 	state: {
 		title: ''
 	},
 	mutations: {
-		rtChangeTitle(state, value) {
+		rtChangeTitle( state, value ) {
 			// mutate state
 			state.title = value;
-			document.title = (state.title ?state. title + ' - ' : '') + rtwp.site_name;
+			document.title = ( state.title ? state.title + ' - ' : '' ) + rtwp.site_name;
 		}
 	}
-})
+} );
 
-//Create instatnce of main component
+//Create instance of main component
 new App( {
 	store,
 	router
-}).$mount('#app');
+} ).$mount( '#app' );
 
