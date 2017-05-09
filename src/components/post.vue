c128ad0d2b186d81cc94498924940b3fe189d056
vishaldodiya <13bce026@nirmauni.ac.in>
2017-05-10 01:04:06 +0530

    Category listing added in post

--- src/components/post.vue
+++ src/components/post.vue
@@ -11,7 +11,9 @@
 
 					<div class="rt-post">
 
-						<h2 class="rt-post-title"> {{ post.title.rendered }}</h2>
+						<h2 class="rt-post-title">{{ post.title.rendered }}</h2>
+
+                        <h4 class="rt-categories" style="display:inline-block;margin-right: 10px;" v-for="item in categories">{{item.name}}</h4>
 
 						<div class="progressive full">
 
@@ -35,7 +37,9 @@
 </template>
 
 <script>
-export default {
+    /* eslint-disable indent */
+
+    export default {
 
 	mounted: function() {
 		this.getPost();
@@ -46,6 +50,7 @@ export default {
 
 			base_path: rtwp.base_path,
 			post: {},
+			categories: [],
 			loaded: 'false',
 			pageTitle: ''
 		};
@@ -63,6 +68,8 @@ export default {
 			.then( ( res ) => {
 
 				vm.post = res.data[ 0 ];
+				this.getCategories( res.data[ 0 ].categories );
+
 				vm.loaded = 'true';
 				vm.pageTitle = vm.post.title.rendered;
 				vm.$store.commit( 'rtChangeTitle', vm.pageTitle );
@@ -74,8 +81,19 @@ export default {
 
 			} );
 
+		},
+        getCategories: function( payload ) {
+            const vm = this;
+            for ( let i = 0; i < payload.length; i++ ) {
+                vm.$http.get( 'wp/v2/categories/' + payload[ i ] )
+                    .then( ( res ) => {
+                        vm.categories.push( res.data );
+                    } )
+                    .catch( ( res ) => {
+                        //console.log( `Something Went Wrong: $(res)` );
+                    } );
+            }
 		}
-
 	}
 };
 </script>
